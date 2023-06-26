7b645b675185486930f9f52978958e4ae9730b17
jason66l <jasons66lee@gmail.com>
2023-06-26 12:53:00 -0700

    counters change color when looked at
    raycasting script

---
+++ chef chas/Assets/Scripts/RaycastView.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastView : MonoBehaviour
+{
+    public Material outlineMaterial;
+    private Renderer hitObjectRenderer;
+    private Material previousMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if ((Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 1.5f)))
+        {
+            GameObject hitObject = hitInfo.collider.gameObject;
+            
+            
+            if (hitObject.tag == ("placeable")){   
+                
+                hitObjectRenderer = hitObject.GetComponent<Renderer>();
+                if (previousMaterial == null)
+                {
+                    previousMaterial = hitObjectRenderer.material;
+                }
+
+                hitObjectRenderer.material = outlineMaterial;
+            }
+        }
+        else
+        {
+
+            if (hitObjectRenderer != null && previousMaterial != null)
+                {
+                    hitObjectRenderer.material = previousMaterial;
+                    previousMaterial = null;
+                
+            }
+        }
+    }
+}

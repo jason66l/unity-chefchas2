300701ea17e3a0a9bc6f4e60218f3af50cdcecac
jason66l <jasons66lee@gmail.com>
2023-06-26 14:29:05 -0700

    place between tables
    super finnicky

--- chef chas/Assets/Scripts/PlateInteraction.cs
+++ chef chas/Assets/Scripts/PlateInteraction.cs
@@ -8,33 +8,64 @@ public class PlateInteraction : MonoBehaviour
     public GameObject playerHands;
     private Rigidbody plateRigidbody;
 
+
+
+
+
+
     void Start(){
         plateRigidbody = GetComponent<Rigidbody>();
     }
     
     void Update(){
     
+  
+
+        //pick up
         if (Pickup.pickUp && inRange){
             
-           transform.position = playerHands.transform.position;
-           plateRigidbody.isKinematic = true;
-           Pickup.handsEmpty = false;
-           print("picked up");
-           
+            transform.position = playerHands.transform.position;
+            plateRigidbody.isKinematic = true;
+            Pickup.handsEmpty = false;
+            print("picked up");
+        
         }
 
+        //drop
         if (!(Pickup.pickUp) ){
             
-           transform.position = transform.position;
-           plateRigidbody.isKinematic = false;
-           Pickup.handsEmpty = true;
-           print("dropped");
-          
+            if (RaycastView.placeOnTable){
+            
+                Vector3 plateHolderPosition = RaycastView.hitObjectTransform.position;
+                plateHolderPosition += new Vector3(0.2f, 2.0f, -0.6f);
+
+                transform.position = plateHolderPosition;
+                plateRigidbody.isKinematic = true;
+                Pickup.handsEmpty = true;
+                print("dropped on table");
+
+                RaycastView.placeOnTable = false;
+            }
+            
+            
+            else{
+                
+                transform.position = transform.position;
+                plateRigidbody.isKinematic = false;
+                Pickup.handsEmpty = true;
+                print("dropped");
+
+                RaycastView.placeOnTable = false;
+
+
+            }
+
+        
 
         }
-
-
     }
+
+    
 
     void OnTriggerEnter(Collider other) {
        
@@ -57,6 +88,7 @@ public class PlateInteraction : MonoBehaviour
 
         }
     }
-    }
+    
     
 
+}
--- chef chas/Assets/Scripts/RaycastView.cs
+++ chef chas/Assets/Scripts/RaycastView.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RaycastView : MonoBehaviour
 {
     public Material outlineMaterial;
+    public static bool placeOnTable;
+    public static Transform hitObjectTransform;
+    
     private Renderer hitObjectRenderer;
     private Material previousMaterial;
 
@@ -17,11 +20,17 @@ public class RaycastView : MonoBehaviour
     {
         if ((Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 1.5f)))
         {
-            GameObject hitObject = hitInfo.collider.gameObject;
             
+            GameObject hitObject = hitInfo.collider.gameObject;
+            hitObjectTransform = hitObject.GetComponent<Transform>();
+            
+            
+            //print(selectedTablePlateHolder.position);
             
             if (hitObject.tag == ("placeable")){   
                 
+                placeOnTable = true;
+
                 hitObjectRenderer = hitObject.GetComponent<Renderer>();
                 if (previousMaterial == null)
                 {
@@ -36,6 +45,7 @@ public class RaycastView : MonoBehaviour
 
             if (hitObjectRenderer != null && previousMaterial != null)
                 {
+                    
                     hitObjectRenderer.material = previousMaterial;
                     previousMaterial = null;
                 
